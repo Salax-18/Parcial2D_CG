ace0414100b5b7ded8a728b573c30935eadf351d
NicolasRHV12 <nicolns.rojas@uao.edu.co>
2026-04-23 22:27:32 -0500

    Squashed commit of the following:
    commit cbb142346f7f5fb0eabf56b957269ae2a1ec9b12
    Merge: 54577d5 716eb01
    Author: Santichorro <sntgtello01@gmail.com>
    Date:   Thu Apr 23 21:41:26 2026 -0500
    
        Merge remote-tracking branch 'origin/Nico' into Santiago
    
    commit 54577d566897d7a6f73163ba201ebbe1614c0b59
    Merge: eebdd8f 105becf
    Author: Santichorro <sntgtello01@gmail.com>
    Date:   Thu Apr 23 21:20:46 2026 -0500
    
        Merge remote-tracking branch 'origin/Johan' into Santiago
    
    commit eebdd8fa68e824b561113322ce164936e5ce0d86
    Author: Santichorro <sntgtello01@gmail.com>
    Date:   Thu Apr 23 21:18:30 2026 -0500
    
        Combate Bosque fin
    
        jijijija
    
    commit 105becf6d12fd16b09a4e62e87f3378f50cd0ec0
    Author: JohanSLopezCh <johan.santiago.lopez.1@gmail.com>
    Date:   Thu Apr 23 21:09:27 2026 -0500
    
        .
    
    commit 10e87b6f6504757bde54753f9bd893202b7a99ed
    Author: JohanSLopezCh <johan.santiago.lopez.1@gmail.com>
    Date:   Thu Apr 23 20:54:41 2026 -0500
    
        .
    
    commit ce7f6013e7c8df3302d0b334b0d796a562856fc5
    Merge: f508995 7f7627e
    Author: Santichorro <sntgtello01@gmail.com>
    Date:   Thu Apr 23 20:48:04 2026 -0500
    
        Merge remote-tracking branch 'origin/Johan' into Santiago
    
    commit 7f7627e396df4978a0146d3587c08d533e3c909e
    Author: JohanSLopezCh <johan.santiago.lopez.1@gmail.com>
    Date:   Thu Apr 23 20:46:47 2026 -0500
    
        .
    
    commit 403b0d9bff47c3d731c34dcbc6d141e685d3350e
    Author: JohanSLopezCh <johan.santiago.lopez.1@gmail.com>
    Date:   Thu Apr 23 20:45:37 2026 -0500
    
        Correccion error TRansicion
    
    commit 8841cc35cbda31968f77f28766952f7d1f721464
    Merge: a1483f4 6b4f086
    Author: JohanSLopezCh <johan.santiago.lopez.1@gmail.com>
    Date:   Thu Apr 23 20:45:18 2026 -0500
    
        Merge remote-tracking branch 'origin/Nico' into Johan

--- P_CG/Assets/Scripts/Scripts_Player/InputLifecycleManager.cs
+++ P_CG/Assets/Scripts/Scripts_Player/InputLifecycleManager.cs
@@ -2,15 +2,47 @@ using UnityEngine;
 
 public class InputLifecycleManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static InputLifecycleManager Instance;
+    private PlayerControls controls;
+
+    private void Awake()
     {
-        
+        // Singleton sencillo para que los jugadores lo encuentren fácil
+        if (Instance == null)
+        {
+            Instance = this;
+            controls = new PlayerControls();
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Los jugadores llamarán a esto para saber hacia dónde moverse
+    public Vector2 GetMovementValue()
     {
-        
+        return controls != null ? controls.Movement.Move.ReadValue<Vector2>() : Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        if (controls != null) controls.Movement.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // Aquí es donde "cerramos la manguera" para evitar el error de Finalize()
+        if (controls != null) controls.Movement.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        if (controls != null)
+        {
+            controls.Dispose();
+            controls = null;
+        }
     }
 }
