d139ae26d0c47aba2443dccc582692b0c2d0b557
JohanSLopezCh <johan.santiago.lopez.1@gmail.com>
2026-04-23 19:07:38 -0500

    cambio minimo
    cambio nombre de la trancicion

--- P_CG/Assets/Scripts/Bosque/TrancicionBosqueCueva.cs
+++ P_CG/Assets/Scripts/Bosque/TrancicionBosqueCueva.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class TrancicionBosque-Cueva : MonoBehaviour
+public class TrancicionBosqueCueva : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
